849966314d09ef2d12e8b4c32d032d7f3bc843f5
unknown <andrey.koshkarov@gmail.com>
2020-12-03 00:23:02 +0200

    workable solution

--- src/calculator.ts
+++ src/calculator.ts
@@ -91,8 +91,88 @@ class Calculator {
     }
 
     calculateWithoutBrackets(parts: string[]): string {
-        // TODO: implement this method
-        return parts[0];
+        let result: string | string[] = parts;
+
+        let multiplyPosition: number;
+        let divisionPosition: number;
+
+        let intermediateResult: string;
+
+        while (typeof result !== "string") {
+            // handle multiply
+            multiplyPosition = result.indexOf(multiply);
+            if (multiplyPosition !== -1) {
+                intermediateResult = (
+                    +result[multiplyPosition - 1] *
+                    +result[multiplyPosition + 1]
+                ).toString();
+
+                if (result.length === 3) {
+                    // no calculations left except current
+                    result = intermediateResult;
+                } else {
+                    result = [
+                        ...result.slice(0, multiplyPosition - 1),
+                        intermediateResult,
+                        ...result.slice(multiplyPosition + 2),
+                    ];
+                }
+                continue;
+            }
+
+            // handle division
+            divisionPosition = result.indexOf(division);
+            if (divisionPosition !== -1) {
+                // TODO: handle division by 0
+                intermediateResult = (
+                    +result[divisionPosition - 1] /
+                    +result[divisionPosition + 1]
+                ).toString();
+
+                if (result.length === 3) {
+                    // no calculations left except current
+                    result = intermediateResult;
+                } else {
+                    result = [
+                        ...result.slice(0, divisionPosition - 1),
+                        intermediateResult,
+                        ...result.slice(divisionPosition + 2),
+                    ];
+                }
+                continue;
+            }
+
+            break;
+        }
+
+        // handle addition and subtraction
+        while (typeof result !== "string") {
+            if (result[1] === plus) {
+                intermediateResult = (+result[0] + +result[2]).toString();
+
+                if (result.length === 3) {
+                    // no calculations left except current
+                    result = intermediateResult;
+                } else {
+                    result = [intermediateResult, ...result.slice(3)];
+                }
+                continue;
+            }
+
+            if (result[1] === minus) {
+                intermediateResult = (+result[0] - +result[2]).toString();
+
+                if (result.length === 3) {
+                    // no calculations left except current
+                    result = intermediateResult;
+                } else {
+                    result = [intermediateResult, ...result.slice(3)];
+                }
+                continue;
+            }
+        }
+
+        return result;
     }
 }
 
@@ -105,6 +185,10 @@ let arguments = [
     ["2 / 2 + 3 * 4 - 6", 7],
     ["( 1 + 2 * 3 ) - 5 / 5", 6],
     ["( 12 + ( 2 * 3 ) - 1 ) + 4", 21],
+    [
+        "( 12 + ( 2 * 3 ) - 1 ) + 4 * ( 2 - 1 * ( 3 - 2 ) ) / ( 4 * 4 / 4 ) + 2",
+        20,
+    ],
 ];
 arguments.forEach(([argument, expected]: [string, number]) => {
     console.log(argument, calculate.evaluate(argument), expected);
